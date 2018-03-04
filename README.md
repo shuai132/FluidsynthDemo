969194b2771f3e4159bb9bbd34db98a86b8b97c2
刘帅 <770722922@qq.com>
2018-03-04 23:27:16 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+This is a demo show how to use fluidsynth in C++ project.
+
+## Requirement
+`cmake`
+
+## Usage
+```
+mkdir build && cd build
+cmake ..
+make
+```
+
+play midi file:
+```
+./midiplayer ../assets/sf/piano.sf2 ../assets/midi/twinkle.mid
+```
+
+## Links
+* [FluidSynth Developer Documentation ](http://www.fluidsynth.org/api/)
---
+++ mymidiplayer.cpp
@@ -0,0 +1,34 @@
+#include "fluidsynth.h"
+#include "MidiFile.h"
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+    if(argc < 3){
+        cout<<"Wrong argument! Usage example:"<<endl;
+        cout<<argv[0]<<" sf.sf2 midi.mid"<<endl;
+        return 1;
+    }
+    const char* sfPath   = argv[1];
+    const char* midiPath = argv[2];
+
+    fluid_settings_t* settings = new_fluid_settings();
+#ifdef __linux__
+    fluid_settings_setstr(settings, "audio.driver", "alsa");
+#endif
+    fluid_synth_t* synth = new_fluid_synth(settings);
+    fluid_audio_driver_t* adriver = new_fluid_audio_driver(settings, synth);
+    fluid_synth_sfload(synth, sfPath, 1);
+    fluid_synth_set_gain(synth, 1.0);
+
+    //todo: 解析midi播放
+
+    /* Clean up */
+    delete_fluid_audio_driver(adriver);
+    delete_fluid_synth(synth);
+    delete_fluid_settings(settings);
+    return 0;
+}
